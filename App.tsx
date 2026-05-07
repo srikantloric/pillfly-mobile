26696f672441f993bb98dc0e1badb88afd922c79
Purna Karmakar <pkarmakar7029@gmail.com>
2026-05-07 21:16:01 +0530

    configure native splash screen and bootsplash setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import RNBootSplash from "react-native-bootsplash";
 
 function App() {
+
+    useEffect(() => {
+    RNBootSplash.hide({ fade: true });
+  }, []); 
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hi there 👋</Text>
